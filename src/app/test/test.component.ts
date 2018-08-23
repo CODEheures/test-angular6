1033ece15683717cef25035ac081a1993073d901
CODEheures <webmaster@codeheures.fr>
2018-08-24 00:13:25 +0200

    add component with bindings tests

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  private msg = {
+    lock: 'Lock all',
+    unlock: 'Unlock All'
+  }
+
+  constructor() { }
+
+  public bindingWord: string
+
+  public isDanger = false
+  public btnMessage: string
+
+  public personnal = {
+    'text-success': !this.isDanger,
+    'text-error': this.isDanger
+  }
+
+  ngOnInit() {
+    this.bindingWord = 'hello'
+    this.btnMessage = this.msg.lock
+  }
+
+  btnClick () {
+    // Invert danger
+    this.isDanger = !this.isDanger
+
+    // Invert btn message
+    this.btnMessage = this.isDanger ? this.msg.unlock : this.msg.lock
+
+    // Invert color
+    this.personnal = {
+      'text-success': !this.isDanger,
+      'text-error': this.isDanger
+    }
+
+    // Add or remove Italic (for test)
+    if (this.isDanger) {
+      const newClassList = Object.assign({}, this.personnal, {'text-italic': true})
+      this.personnal = newClassList
+    } else {
+      if ('text-italic' in this.personnal) { delete this.personnal['text-italic'] }
+    }
+  }
+
+}
