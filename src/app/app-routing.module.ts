0dec71abc9901e8cea72c458134fbb8db0282668
CODEheures <webmaster@codeheures.fr>
2018-08-26 19:37:20 +0200

    page wildcard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './page/users/users.component';
 import { TodosComponent } from './page/todos/todos.component';
 import { HomeComponent } from './page/home/home/home.component';
+import { Page404Component } from './page/404/page404/page404.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
+  { path: '', redirectTo: '/home',  pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
   { path: 'users', component: UsersComponent},
-  { path: 'todos', component: TodosComponent}
+  { path: 'todos', component: TodosComponent},
+  { path: '**', component: Page404Component}
 ];
 
 @NgModule({
@@ -15,4 +18,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomeComponent, UsersComponent, TodosComponent]
+export const routingComponents = [Page404Component, HomeComponent, UsersComponent, TodosComponent]
